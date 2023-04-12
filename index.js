2e5e3d4c3a3e59f3e82f56027dfa018cf5e8032f
Serenity Parker <sarenity.parker01@gmail.com>
2023-04-12 11:13:50 -0500

    help

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun() {
+    if (saturdayFun)
+    return ("This Saturday, I want to roller-skate!");
+    else ("bathe my dog")
+    return ("This Saturday, I want to bathe my dog!")
+
+}
+
+function mondayWork (){
+
+}
+
+function wrapAdjective(){
+
+}
